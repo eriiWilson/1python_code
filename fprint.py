d0469246bb1f1dc89772741976727e2642301dc4
Eric Wilson <eric_wils0n@hotmail.com>
2024-03-18 11:00:20 -0300

    python bruh

---
+++ fprint.py
@@ -0,0 +1,11 @@
+print ('python na escola de programação alura')
+
+nome = input('qual seu nome? ')
+idadade = input('quantos anos tem? ')
+print (f'meu nome é {nome}, e tenho {idadade} anos')
+
+print('A','L','U','R','A',sep='\n')
+
+
+pi = 3.14159
+print (f'o valor de pi arredondado é', round(pi, 2))
